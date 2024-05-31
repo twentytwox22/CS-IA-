ba4723a70773f5414c0b96f142876625cec3d54c
junrui <limjrui@gmail.com>
2024-05-31 16:48:06 +1000

    just removing some comments

--- dbConfig.js
+++ dbConfig.js
@@ -3,11 +3,11 @@ const {Pool} = require ("pg");
 const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  user: 'postgres', // Your PostgreSQL username
-  host: 'localhost', // Your PostgreSQL server host
-  database: 'csia', // Your PostgreSQL database name
-  password: 'test', // Your PostgreSQL password
-  port: 5432, // Your PostgreSQL server port
+  user: 'postgres', 
+  host: 'localhost', 
+  database: 'csia', 
+  password: 'test',
+  port: 5432, 
 });
 
 module.exports = {pool};
--- passportConfig.js
+++ passportConfig.js
@@ -50,22 +50,18 @@ function initialize(passport) {
       authenticateUser
     )
   );
-  // Stores user details inside session. serializeUser determines which data of the user
-  // object should be stored in the session. The result of the serializeUser method is attached
-  // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
-  //   the user id as the key) req.session.passport.user = {id: 'xyz'}
-  passport.serializeUser((student, done) => done(null, student.student_id));
 
+passport.serializeUser((student, done) => done(null, student.student_id));
 
-  // In deserializeUser that key is matched with the in memory array / database or any data resource.
-  // The fetched object is attached to the request object as req.user
-  passport.deserializeUser((student_ID, done) => {
-    pool.query(`SELECT * FROM students WHERE student_ID = $1`, [student_ID], (err, results) => {
-      if (err) {
-        return done(err);
+// In deserializeUser that key is matched with the in memory array / database or any data resource.
+// The fetched object is attached to the request object as req.user
+passport.deserializeUser((student_ID, done) => {
+  pool.query(`SELECT * FROM students WHERE student_ID = $1`, [student_ID], (err, results) => {
+    if (err) {
+      return done(err);
       }
-      console.log(`ID is ${student_ID}`);
-      return done(null, results.rows[0]);
+    console.log(`ID is ${student_ID}`);
+    return done(null, results.rows[0]);
     });
   });
 }
